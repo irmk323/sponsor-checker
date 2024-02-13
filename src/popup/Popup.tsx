4a290c321d626566b0709952572387f8ef1bd65e
Orbay A <orbay.altuntoprak@gmail.com>
2024-02-13 13:56:37 +0300

    save mi shit

--- src/popup/Popup.tsx
+++ src/popup/Popup.tsx
@@ -1,10 +1,41 @@
+import { Card, Switch, Text, Tabs, rem } from '@mantine/core';
+import { IconSettings } from '@tabler/icons-react';
+
 import React from "react";
-import { Card, Switch, Text } from '@mantine/core';
 import { isExtensionEnabled, setIsExtensionEnabled } from "../storage/localStorage"
 
-export const SettingsPage: React.FC<{}> = () => {
+const TABS = {
+    search: "search",
+    settings: "settings",
+} as const;
+
+export const PopupPage: React.FC<{}> = () => {
+  const iconStyle = { width: rem(12), height: rem(12) };
+  const [activeTab, setActiveTab] = React.useState<string>(TABS.search);
+  return (
+    <Card withBorder shadow="sm" radius="md" className="main-page">
+
+      <Tabs color="indigo" value={activeTab} onChange={val => val != null && setActiveTab(val)}>
+        <Tabs.List>
+          <Tabs.Tab value={TABS.search}>Search</Tabs.Tab>
+          <Tabs.Tab value={TABS.settings} leftSection={<IconSettings style={iconStyle}/>}>Settings</Tabs.Tab>
+        </Tabs.List>
+
+        <Tabs.Panel value={TABS.search}>
+          Gallery tab content
+        </Tabs.Panel>
+
+        <Tabs.Panel value={TABS.settings}>
+          <SettingsPage />
+        </Tabs.Panel>
+      </Tabs>
+    </Card>
+  );
+}
+
+const SettingsPage: React.FC<{}> = () => {
     return (
-        <Card withBorder shadow="sm" radius="md" className="main-card">
+        <Card withBorder shadow="sm" radius="md">
             <Card.Section withBorder inheritPadding py="xs">
                 <Text fw={500}>Settings</Text>
             </Card.Section>
